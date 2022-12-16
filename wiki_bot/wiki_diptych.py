b58adec3b025350935218252919751329144bb9e
lilblizzard <lilblizzard97@gmail.com>
2022-12-16 18:02:30 -0500

    modify to run in an AWS Lambda function

--- wiki_bot/wiki_diptych.py
+++ wiki_bot/wiki_diptych.py
@@ -1,22 +1,24 @@
+import requests
 import wikipedia
 import tweepy
-import random
-import urllib.request
+from io import BytesIO
 from PIL import Image
+import boto3
 import os
-from dotenv import load_dotenv
+import uuid
 
-load_dotenv()
+
+aws_access_key_id = os.environ['AWS_ACCESS']
+aws_secret_access_key = os.environ['AWS_SECRET']
 
 
 # defining our twitter api
 def twitter_api():
+    twitter_api_key = os.environ["TWITTER_API_KEY"]
+    twitter_api_secret = os.environ["TWITTER_API_SECRET"]
 
-    twitter_api_key = os.getenv("TWITTER_API_KEY")
-    twitter_api_secret = os.getenv("TWITTER_API_SECRET")
-
-    twitter_access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-    twitter_access_secret = os.getenv("TWITTER_ACCESS_SECRET")
+    twitter_access_token = os.environ["TWITTER_ACCESS_TOKEN"]
+    twitter_access_secret = os.environ["TWITTER_ACCESS_SECRET"]
 
     auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret)
     auth.set_access_token(twitter_access_token, twitter_access_secret)
@@ -24,9 +26,9 @@ def twitter_api():
     api = tweepy.API(auth)
     return api
 
-def get_page_with_at_least_two_jpegs():
 
-    #start the count at 0
+def get_page_with_at_least_two_jpegs():
+    # start the count at 0
     count_of_jpegs = 0
     while count_of_jpegs < 2:
         try:
@@ -50,7 +52,7 @@ def get_page_with_at_least_two_jpegs():
             # count jpg images from urls
             for url in image_urls:
                 if url[-4:] == '.jpg':
-                    count_of_jpegs+=1
+                    count_of_jpegs += 1
                     jpeg_url = url
         except wikipedia.exceptions.DisambiguationError:
             print('DisambiguationError')
@@ -62,22 +64,48 @@ def get_page_with_at_least_two_jpegs():
     # return the title & url of page with more than 2 jpegs
     return random_page.title, jpeg_url
 
+
+def prepare_image_path(pair):
+    # this function will take in an array [title, image_url]
+    # store image data in memory
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/39.0.2171.95 Safari/537.36'}
+    response = requests.get(pair[1], headers=headers)
+    img = BytesIO(response.content)
+    # connect to s3 bucket
+    bucket = 'wiki-diptych-bucket'
+    s3 = boto3.client('s3',
+                      aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key)
+    # upload image to s3
+    img_name = f'{uuid.uuid4().hex}.jpg'
+    s3.upload_fileobj(img, bucket, img_name)
+    # download into lambda tmp directory
+    s3_resource = boto3.resource('s3',
+                                 aws_access_key_id=aws_access_key_id,
+                                 aws_secret_access_key=aws_secret_access_key)
+    s3_resource.Bucket(bucket).download_file(f'{img_name}', f'/tmp/{img_name}')
+    # delete from s3
+    s3.delete_object(Bucket=bucket, Key=img_name)
+    # test and downscale that image in tmp if too large
+    return [pair[0], test_image_and_downscale_if_too_large(f'/tmp/{img_name}')]
+    # return array [title, image_path]
+
+
 # reads inputs from the title & url pairs of 2 pages
 def assemble_tweet(first_pair, second_pair):
-
     success = False
     while not success:
         try:
             # download the image and store it locally
-            urllib.request.urlretrieve(first_pair[1], '/home/pi/wiki_diptych/wiki_bot/first_image.jpg')
-            urllib.request.urlretrieve(second_pair[1], '/home/pi/wiki_diptych/wiki_bot/second_image.jpg')
+            img1_path = first_pair[1]
+            img2_path = second_pair[1]
 
-            #upload our images to twitter & update our media_ids
-            filenames = ['/home/pi/wiki_diptych/wiki_bot/first_image.jpg','/home/pi/wiki_diptych/wiki_bot/second_image.jpg']
+            # upload our images to twitter & update our media_ids
+            filenames = [img1_path, img2_path]
             media_ids = []
             for filename in filenames:
-                #test size of images, return appropraite file
-                test_image_and_downscale_if_too_large(filename)
                 try:
                     res = twitter_api().media_upload(filename)
                     media_ids.append(res.media_id)
@@ -86,34 +114,34 @@ def assemble_tweet(first_pair, second_pair):
                     continue
 
             # tweet names of articles & pictures
-            twitter_api().update_status(status = first_pair[0] + ', ' + second_pair[0], media_ids=media_ids)
+            twitter_api().update_status(status=first_pair[0] + ', ' + second_pair[0], media_ids=media_ids)
             success = True
 
-        #error handling
+        # error handling
         except tweepy.error.TweepError as e:
             print(e)
 
 
 # test if our image is too large for tweepy to send it
 def test_image_and_downscale_if_too_large(filepath):
-
-    #if the image is < 2000kb, return image
+    # if the image is < 2000kb, return image
     image_size = os.stat(filepath).st_size
     success = False
     while not success:
         if image_size < 2000000:
             success = True
             return filepath
-        #if the image is > 2000kb, return resized image and replace file
+        # if the image is > 2000kb, return resized image and replace file
         else:
             original_image = Image.open(filepath)
             original_image.save(filepath, quality=20, optimized=True)
             return filepath
 
-#main loop
-first_pair = get_page_with_at_least_two_jpegs()
-second_pair = get_page_with_at_least_two_jpegs()
-print(first_pair)
-print(second_pair)
-assemble_tweet(first_pair,second_pair)
 
+# main loop
+def lambda_handler(event, context):
+    first_pair = get_page_with_at_least_two_jpegs()
+    second_pair = get_page_with_at_least_two_jpegs()
+    print(first_pair)
+    print(second_pair)
+    assemble_tweet(prepare_image_path(first_pair), prepare_image_path(second_pair))
